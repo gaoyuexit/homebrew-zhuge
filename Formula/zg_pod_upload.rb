e5e73ec7d979397890623bbc04bc89ce90e369dd
gaoyu <gaoyu@zhugefang.com>
2018-12-01 15:42:00 +0800

    修改1.2版本的下载地址

--- Formula/zg_pod_upload.rb
+++ Formula/zg_pod_upload.rb
@@ -4,8 +4,8 @@
 class ZgPodUpload < Formula
   desc "a simple tool for update cocoapods private repo"
   homepage "http://gaoyu.in"
-  url "https://github.com/gaoyuexit/homebrew-zhuge/blob/master/zg_pod_upload/zg_pod_upload-1.1.tar.xz?raw=true"
-  sha256 "51f569524e1312de6da6a6f7f9216f93af055c3d59d9d6a2e6ebcb2c0a93be2a"
+  url "https://github.com/gaoyuexit/homebrew-zhuge/blob/master/zg_pod_upload/zg_pod_upload-1.2.tar.xz?raw=true"
+  sha256 "8a5f3a07941a6c5ea8a80ed62a3d9d7fb1d1f07a6d7a7cf985ed8ebf7eebcb6d"
   # depends_on "cmake" => :build
 
   def install
